d3022d73131c4785a319ff9e1c8de63aa1ec230a
jfwf <jfwf@yeah.net>
2020-02-13 23:00:39 +0800

    2020-2-13
    找到bug所在了，因为fwrite写入stdout时，win以ascii的形式解读这些二进制，会在0A字符后面加0D也就时换行符。

--- cgitiles.cpp
+++ cgitiles.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 //0,1...,9,-   3x7x11
-int DigitTemplate[] = {1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,  //48
+int DigitTemplate[] = { 1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,  //48
 						0,0,1,0,1,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1, //49
 						1,1,1,0,0,1,0,0,1,1,1,1,1,0,0,1,0,0,1,1,1,
 						1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,0,0,1,1,1,1,
@@ -32,7 +32,7 @@ int DigitTemplate[] = {1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,  //48
 /// digiStr - digit char array
 /// backVal - background value
 /// frontVal - foreground value
-void drawdigits(int x0,int y0,int weight,int imgWid,int imgHei,short* imgData, char* digiStr ,int backVal, int frontVal)
+void drawdigits(int x0,int y0,int weight,int imgWid,int imgHei,short* imgData,const char* digiStr ,int backVal, int frontVal)
 {
 	weight = max(1,weight) ;
 	//every digit is 3x7
@@ -78,11 +78,16 @@ int main(int argc, char* argv[])
 
 	LIST *head = cgi_input_parse();
 	if( head ){
-		char* xval =  find_val(head,"x") ;
-		char* yval =  find_val(head,"y") ;
-		char* zval =  find_val(head,"z") ;
+		//char* xval =  find_val(head,"x") ;
+		//char* yval =  find_val(head,"y") ;
+		//char* zval =  find_val(head,"z") ;
 		char* v0val = find_val(head,"v0") ;
 		char* v1val = find_val(head,"v1") ;
+
+		const char* xval = "1" ;
+		const char* yval = "1" ;
+		const char* zval = "1" ;
+
 
 		int v0i = 0 ;
 		int v1i = 255 ;
@@ -94,9 +99,9 @@ int main(int argc, char* argv[])
 
 		if( xval && yval && zval )
 		{
-			cout << "Content-type:application/octet-stream\r\n\r\n";
+			cout << "Content-type:application/octet-stream; charset=x-user-defined\nContent-Length:131072\nAccept-Ranges: bytes\r\n\r\n";
 			
-			float dpx = (v1i - v0i ) / 255.f ;
+			int dpx = (v1i - v0i ) / 255  ;
 			
 			int itilex = atof(xval) ;
 			int itiley = atof(yval) ;
@@ -104,10 +109,14 @@ int main(int argc, char* argv[])
 			const int imgSize = 256*256 ;
 			short* tdata = new short[imgSize] ;
 			for(int iy = 0 ; iy < 256 ; ++ iy ){
-				int pxval = v0i + iy * dpx ;
+				int pxval = v0i + iy * 2 ;
 				for(int ix = 0 ; ix < 256 ; ++ ix )
 				{
-					tdata[ix+iy*256] =v0i;// (short) pxval ; //here 这个地方有bug找不到！！！
+					tdata[ix+iy*256]= (short) pxval ; //here 这个地方有bug找不到！！！
+					//找到问题所在了，MSVC2010在fwrite二进制数据到stdout中，stdout会把二进制数据当初文本解读，
+					//而0A 00 为短整型值10，其0A作为ASCII码正好是LF换行符的意思，win系统的stdout遇到这个换行符，
+					//自动加上0D 也就是CR回车符，奇怪的行为，但是win就是这么做的，所以造成最后输出的数据凭空增加了若干字节，
+					//作为2字节解读的时候也会解读错误。解决方法就是往stdout输出的时候不要以文本的形式输出，以二进制形式输出。
 				}
 			}
 			
@@ -116,6 +125,12 @@ int main(int argc, char* argv[])
 			//drawdigits(0,27+27,3,256,256,tdata,zval,v0i,v1i) ;
 			
 			fwrite( tdata , 2, imgSize, stdout);
+			
+			char tempfilename[32] ;
+			sprintf(tempfilename,"tiledata_%d_%d",v0i,v1i) ;
+			FILE* fp = fopen(tempfilename,"wb") ;
+			fwrite(tdata,2,256*256,fp) ;
+			fclose(fp) ;
 			delete[] tdata ;
 		}
 	}
